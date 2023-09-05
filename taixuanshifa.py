83fbfa71b8696f34cf5190087611309e549c5f2b
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-09-05 23:46:55 +0800

    Update taixuanshifa.py

--- taixuanshifa.py
+++ taixuanshifa.py
@@ -1,43 +1,37 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 28 12:43:10 2020
-
+Updated on Tue Sep 5 23:46 2023
 @author: Ken Tang
 @email: kinyeah@gmail.com
 """
 
-import random, pickle, os
+import random, pickle, os, datetime, itertools
+from sxtwl import fromSolar, fromLunar
+import cnlunar
+from cn2an import an2cn
+
+def new_list(olist, o):
+    a = olist.index(o)
+    res1 = olist[a:] + olist[:a]
+    return res1
+
+def gen_clist(su,num):
+    return [su+b for b in [an2cn(str(i)) for i in list(range(num+1))][1:]]
 
 root = os.path.abspath(os.path.dirname(__file__))
 data = os.path.join(root, 'data')
 taixuandict_path  = os.path.join(root, 'data', 'taixuandict.p')
 taixuandict = pickle.load( open( taixuandict_path, "rb" ))
 chin_list = list('角亢氐房心尾箕斗牛女虛危室壁奎婁胃昴畢觜參井鬼柳星張翼軫')
-yearsu = [item for sublist in [[chin_list[i]]* [12,9,15,5,5,18,11,26,8,12,10,17,16,9,16,12,14,11,16,2,9,33,4,15,7,18,18,17][i] for i in range(0,28)] for item in sublist]
+a = new_chinlist = new_list(chin_list, "牛")
+b = [8,12,10,17,16,9,16,12,14,11,16,2,9,33,4,15,7,18,18,17,12,9,15,5,5,18,11,26]
+cnum = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二', '十三', '十四', '十五', '十六', '十七', '十八', '十九', '二十', '二十一', '二十二', '二十三', '二十四', '二十五', '二十六', '二十七', '二十八', '二十九', '三十', '三十一', '三十二', '三十三']
+c =dict(zip(b,a))
+yearsu = list(itertools.chain.from_iterable([gen_clist(c.get(i),i) for i in b]))
 
-def jiazi():
-    Gan, Zhi = '甲乙丙丁戊己庚辛壬癸', '子丑寅卯辰巳午未申酉戌亥'
-    return list(map(lambda x: "{}{}".format(Gan[x % len(Gan)], Zhi[x % len(Zhi)]), list(range(60))))
 
 
-def jq(year, month, day):
-    dd = fromSolar(year, month, day) 
-    jqmc = ["冬至", "小寒", "大寒", "立春", "雨水", "驚蟄", "春分", "清明", "谷雨", "立夏",
-         "小滿", "芒種", "夏至", "小暑", "大暑", "立秋", "處暑","白露", "秋分", "寒露", "霜降", 
-         "立冬", "小雪", "大雪"]
-    while True:
-        dd = dd.before(1)
-        if dd.hasJieQi():
-            return jqmc[dd.getJieQi()]
-            break
-        
-def getjq(year, month, day):
-    dd = fromSolar(year, month, day) 
-    jqmc = ["冬至", "小寒", "大寒", "立春", "雨水", "驚蟄", "春分", "清明", "谷雨", "立夏",
-         "小滿", "芒種", "夏至", "小暑", "大暑", "立秋", "處暑","白露", "秋分", "寒露", "霜降", 
-         "立冬", "小雪", "大雪"]
-    return  dd.getJieQi()
-    
 
 def multi_key_dict_get(d, k):
     for keys, v in d.items():
@@ -45,51 +39,116 @@ def multi_key_dict_get(d, k):
             return v
     return None
 
+class Taixuan:
+    def __init__(self, year, month, day, hour):
+        self.year, self.month, self.day, self.hour = year, month, day, hour
 
-def qigua_number():
-    wai_dict = {7:1, 8:2, 9:3}
-    shifa_results = []
-    shifa_results2 = []
-    for i in range(4):
-        stalks_first = 36 - 3 - 1 #36策，虛三，掛一
-        dividers = sorted(random.sample(range(25, stalks_first), 1))
-        two_division  = [a - b for a, b in zip(dividers + [stalks_first+10], [10] + dividers)]
-        two_division_left = two_division[0] % 3 
-        if two_division_left == 0:
-            two_division_left = 3
-        two_division_right = two_division[1] % 3 
-        if two_division_right == 0:
-            two_division_right = 3
-        stalks_second = stalks_first - two_division_left - two_division_right
-        dividers_second = sorted(random.sample(range(25, stalks_second), 1))
-        two_division_second  = [a - b for a, b in zip(dividers_second + [stalks_second+10], [10] + dividers_second)]
-        two_division_left_second = two_division_second [0] % 3 
-        if two_division_left_second == 0:
-            two_division_left_second = 3
-        two_division_right_second = two_division_second [1] % 3 
-        if two_division_right_second == 0:
-            two_division_right_second = 3
-        stalks_third = stalks_second - two_division_left_second - two_division_right_second
-        wai = stalks_third / 3 
-        shifa_results.append(wai_dict.get(wai))
-        shifa_results2.append(wai)
-    zhan = (shifa_results2[0] + shifa_results2[1] + shifa_results2[2] + shifa_results2[3]) % 9 
-    if zhan == 0:
-        zhan = 9
-    zhou = "".join(str(e) for e in shifa_results[:4])
-    return zhou, int(zhan)
+    def jq(self, year, month, day):
+        dd = fromSolar(self.year, self.month, self.day) 
+        jqmc = ["冬至", "小寒", "大寒", "立春", "雨水", "驚蟄", "春分", "清明", "谷雨", "立夏",
+             "小滿", "芒種", "夏至", "小暑", "大暑", "立秋", "處暑","白露", "秋分", "寒露", "霜降", 
+             "立冬", "小雪", "大雪"]
+        while True:
+            dd = dd.before(1)
+            if dd.hasJieQi():
+                return jqmc[dd.getJieQi()]
+                break
+            
+    def getdz_date(self):
+        pyear = self.year -1 
+        pdz_m_d = cnlunar.Lunar(datetime.datetime(pyear, self.month, self.day, self.hour, 0)).thisYearSolarTermsDic.get("冬至")
+        pdz = datetime.datetime(pyear, pdz_m_d[0], pdz_m_d[1])
+        dz_m_d = cnlunar.Lunar(datetime.datetime(self.year, self.month, self.day, self.hour, 0)).thisYearSolarTermsDic.get("冬至")
+        dz = datetime.datetime(self.year, dz_m_d[0], dz_m_d[1])
+        currentday = datetime.datetime(self.year, self.month, self.day)
+        daydifference = (currentday - dz).days
+        if daydifference < 0 :
+            return pdz
+        else:
+            return dz
+            
+    def getdz(self):
+        pyear = self.year -1 
+        pdz_m_d = cnlunar.Lunar(datetime.datetime(pyear, self.month, self.day, self.hour, 0)).thisYearSolarTermsDic.get("冬至")
+        pdz = datetime.datetime(pyear, pdz_m_d[0], pdz_m_d[1])
+        dz_m_d = cnlunar.Lunar(datetime.datetime(self.year, self.month, self.day, self.hour, 0)).thisYearSolarTermsDic.get("冬至")
+        dz = datetime.datetime(self.year, dz_m_d[0], dz_m_d[1])
+        currentday = datetime.datetime(self.year, self.month, self.day)
+        daydifference = (currentday - dz).days
+        if daydifference < 0 :
+            return (currentday - pdz).days
+        else:
+            return daydifference
+        
+    def qigua_number(self):
+        wai_dict = {7:1, 8:2, 9:3}
+        shifa_results = []
+        shifa_results2 = []
+        for i in range(4):
+            stalks_first = 36 - 3 - 1 #36策，虛三，掛一
+            dividers = sorted(random.sample(range(25, stalks_first), 1))
+            two_division  = [a - b for a, b in zip(dividers + [stalks_first+10], [10] + dividers)]
+            two_division_left = two_division[0] % 3 
+            if two_division_left == 0:
+                two_division_left = 3
+            two_division_right = two_division[1] % 3 
+            if two_division_right == 0:
+                two_division_right = 3
+            stalks_second = stalks_first - two_division_left - two_division_right
+            dividers_second = sorted(random.sample(range(25, stalks_second), 1))
+            two_division_second  = [a - b for a, b in zip(dividers_second + [stalks_second+10], [10] + dividers_second)]
+            two_division_left_second = two_division_second [0] % 3 
+            if two_division_left_second == 0:
+                two_division_left_second = 3
+            two_division_right_second = two_division_second [1] % 3 
+            if two_division_right_second == 0:
+                two_division_right_second = 3
+            stalks_third = stalks_second - two_division_left_second - two_division_right_second
+            wai = stalks_third / 3 
+            shifa_results.append(wai_dict.get(wai))
+            shifa_results2.append(wai)
+        zhan = (shifa_results2[0] + shifa_results2[1] + shifa_results2[2] + shifa_results2[3]) % 9 
+        if zhan == 0:
+            zhan = 9
+        zhou = "".join(str(e) for e in shifa_results[:4])
+        return zhou, int(zhan), shifa_results
+    
+    def qigua(self):
+        results = self.qigua_number()
+        gua_number = int(results[0])
+        result = results[2]
+        gua_details = taixuandict.get(gua_number)
+        gua = gua_details.get("卦")
+        #zhan_dict = {1:"初一", 2:"次二", 3:"次三", 4:"次四", 5:"次五", 6:"次六", 7:"次七", 8:"次八", 9:"上九"}
+        zhan_number = results[1]
+        #zhan = gua_details.get(zhan_number)
+        daynightselect = {"旦":["初一","次五","次七"], "夕":["次三","次四","次八"], "日中":["次二","次六","上九"], "夜中":["次二","次六","上九"]}
+        hours = list(range(24))
+        currenttime = multi_key_dict_get({tuple(hours[6:12]):"旦", tuple(hours[12:18]):"日中", tuple(hours[18:24]):"夕", tuple(hours[0:6]):"夜中"},self.hour)
+        dnn = daynightselect.get(currenttime)
+        cnum = {1:"一", 2:"二", 3:"三"}
+        cr = [cnum.get(i) for i in result]
+        head = "{}方{}州{}部{}家".format(cr[3], cr[2], cr[1], cr[0])
+        xzlist = {"一家":1, "二家":2, "三家":3, "一部":0, "二部":3, "三部":6, "一州":0, "二州":9, "三州":18, "一方":0, "二方":27, "三方":54}
+        xuan_head = xzlist.get(head[0:2]) + xzlist.get(head[2:4]) + xzlist.get(head[4:6]) + xzlist.get(head[6:8])
+        zhan = (xuan_head-1) * 9  
+        biao = (xuan_head-1) * 3  
+        xuan_zan = zhan // 2 
+        su = dict(zip(list(range(365)),yearsu)).get(xuan_zan)
+        return {
+            "方州部家":head,
+            "爻排列":gua_number,
+            "太玄卦": gua,
+            "玄首":xuan_head,
+            "玄贊":zhan,
+            "表贊":biao,
+            "星宿": su+"度",
+            "起筮時段": currenttime,
+            "爻辭":[{i:gua_details.get(i)} for i in dnn],
+            }
 
-def qigua():
-    gua_number = int(qigua_number()[0])
-    gua_details = taixuandict.get(gua_number)
-    gua = gua_details.get("卦")
-    zhan_dict = {1:"初一", 2:"次二", 3:"次三", 4:"次四", 5:"次五", 6:"次六", 7:"次七", 8:"次八", 9:"上九"}
-    zhan_number = zhan_dict.get(qigua_number()[1])
-    zhan = gua_details.get(zhan_number)
-    daynightselect = {"旦":["初一","次五","次七"], "夕":["次三","次四","次八"], "日中":["次二","次六","上九"], "夜中":["次二","次六","上九"]}
-    hours = list(range(24))
-    hour = datetime.now().hour
-    dnn = daynightselect.get( multi_key_dict_get({tuple(hours[6:12]):"旦", tuple(hours[12:18]):"日中", tuple(hours[18:24]):"夕", tuple(hours[0:6]):"夜中"},hour))
-    return gua_number, gua, [{i:gua_details.get(i)} for i in dnn], qigua_number()
 
-
+if __name__ == '__main__':
+    print(Taixuan(2020,4,24,0).qigua())
+   
+    
