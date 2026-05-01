6d972e9020a71d846da7e55e37dc759010c24f18
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2026-05-01 09:10:45 +0800

    Update taixuanshifa.py

--- taixuanshifa.py
+++ taixuanshifa.py
@@ -148,12 +148,8 @@ class Taixuan:
         gb = divine_yy.get( currenttime  + xuan_head_oe)
         zhan = (xuan_head-1) * 9  
         biao = (xuan_head-1) * 3  
-        days_since_dz = self.getdz()
-        shichen_index = (self.hour + 1) // 2 % 12  # 0=子,1=丑,...,11=亥
-        # At 午時(index 6) the sun is on the meridian; each 時辰 the meridian
-        # advances ~365/12 ≈ 30.4 Chinese degrees due to Earth's rotation.
-        shichen_offset = round((shichen_index - 6) * 365 / 12)
-        su = dict(zip(list(range(365)),yearsu)).get((days_since_dz + shichen_offset) % 365)
+        xuan_zan = zhan // 2 
+        su = dict(zip(list(range(365)),yearsu)).get(xuan_zan)
         pan1 = "起卦時間︰{}年{}月{}日{}時\n".format(self.year, self.month, self.day, self.hour)
         a = cnlunar.Lunar(datetime.datetime(self.year, self.month, self.day, self.hour, 0))
         pan2 = "農　　曆︰%s年%s%s日\n" % (a.lunarYearCn,  a.lunarMonthCn[:-1], a.lunarDayCn)
